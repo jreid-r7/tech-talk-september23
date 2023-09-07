2548fd12705b5f195534cb8844f50fdb506c067c
John Reid <jreid@rapid7.com>
2023-09-07 13:39:13 +0100

    Iniitial commit

---
+++ src/main/java/com/rapid7/ipims/techtalk/techtalk/Person.java
@@ -0,0 +1,23 @@
+package com.rapid7.ipims.techtalk.techtalk;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+}
---
+++ src/test/java/com/rapid7/ipims/techtalk/techtalk/TechtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapid7.ipims.techtalk.techtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
